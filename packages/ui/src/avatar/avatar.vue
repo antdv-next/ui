aaa70ebce3279abde9bb8c812dbaf6f363ccf158
carl-chen <ccheartszzz@gmail.com>
2025-09-17 18:38:00 +0800

    wip

--- packages/ui/src/avatar/avatar.vue
+++ packages/ui/src/avatar/avatar.vue
@@ -1,10 +1,26 @@
 <script lang="ts" setup>
 import type { AvatarProps } from './define'
+import { computed, ref, shallowRef } from 'vue'
+import { useConfigContext } from '../config-provider/context'
+
+defineOptions({
+  name: 'AAvatar',
+  inheritAttrs: false,
+})
 
 const props = defineProps<AvatarProps>()
+
+const isImgExist = ref(true)
+const isMounted = ref(false)
+const scale = shallowRef(1)
+
+const avatarChildrenRef = shallowRef<HTMLElement | null>(null)
+const avatarNodeRef = shallowRef<HTMLElement | null>(null)
+
+const ctx = useConfigContext()
+const prefixCls = computed(() => ctx.getPrefixCls('avatar', props.prefixCls))
 </script>
 
-  <template>
-  <div>
-  </div>
-  </template>
+<template>
+  <div />
+</template>
